f40362cd80b82aa7346c73815af552f068046832
superestos <superestos@gmail.com>
2023-05-02 22:42:52 +0800

    basic kernel modules skel

--- tools/labs/skels/kernel_modules/4-multi-mod/mod2.c
+++ tools/labs/skels/kernel_modules/4-multi-mod/mod2.c
@@ -1,3 +1,11 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+
+MODULE_DESCRIPTION("add module");
+MODULE_AUTHOR("Kernel Hacker");
+MODULE_LICENSE("GPL");
+
 int add(int a, int b)
 {
 	return a + b;
--- tools/labs/skels/kernel_modules/7-list-proc/list_proc.c
+++ tools/labs/skels/kernel_modules/7-list-proc/list_proc.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 /* TODO: add missing headers */
+#include <linux/sched.h>
 
 MODULE_DESCRIPTION("List current processes");
 MODULE_AUTHOR("Kernel Hacker");
@@ -9,11 +10,8 @@ MODULE_LICENSE("GPL");
 
 static int my_proc_init(void)
 {
-	struct task_struct *p;
-
 	/* TODO: print current process pid and its name */
-
-	/* TODO: print the pid and name of all processes */
+	pr_info("The process is \"%s\" (pid %i)\n", current->comm, current->pid);
 
 	return 0;
 }
@@ -21,6 +19,7 @@ static int my_proc_init(void)
 static void my_proc_exit(void)
 {
 	/* TODO: print current process pid and name */
+	pr_info("The process is \"%s\" (pid %i)\n", current->comm, current->pid);
 }
 
 module_init(my_proc_init);
