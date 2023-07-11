a4682bb9d1c86b02509059be802a8b9209c36e59
superestos <superestos@gmail.com>
2023-07-12 01:16:25 +0800

    register block device && handle request

--- tools/labs/skels/block_device_drivers/1-2-3-6-ram-disk/kernel/ram-disk.c
+++ tools/labs/skels/block_device_drivers/1-2-3-6-ram-disk/kernel/ram-disk.c
@@ -86,12 +86,20 @@ static blk_status_t my_block_request(struct blk_mq_hw_ctx *hctx,
 	struct my_block_dev *dev = hctx->queue->queuedata;
 
 	/* TODO 2: get pointer to request */
+	rq = bd->rq;
 
 	/* TODO 2: start request processing. */
+	blk_mq_start_request(rq);
 
 	/* TODO 2: check fs request. Return if passthrough. */
+	if (blk_rq_is_passthrough(rq)) {
+		pr_info("skip non-fs request\n");
+		blk_mq_end_request(rq, BLK_STS_IOERR);
+		goto out;
+	}
 
 	/* TODO 2: print request information */
+	pr_info("request received: start sector: %llu data size: %u\n", rq->__sector, rq->__data_len);
 
 #if USE_BIO_TRANSFER == 1
 	/* TODO 6: process the request by calling my_xfer_request */
@@ -100,6 +108,7 @@ static blk_status_t my_block_request(struct blk_mq_hw_ctx *hctx,
 #endif
 
 	/* TODO 2: end request successfully */
+	blk_mq_end_request(rq, BLK_STS_OK);
 
 out:
 	return BLK_STS_OK;
@@ -179,13 +188,23 @@ static int __init my_block_init(void)
 	int err = 0;
 
 	/* TODO 1: register block device */
+	err = register_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
+	if (err < 0) {
+		pr_err("unable to register mybdev block device\n");
+		return err;
+	}
 
 	/* TODO 2: create block device using create_block_device */
+	err = create_block_device(&g_dev);
+	if (err < 0) {
+		goto out;
+	}
 
 	return 0;
 
 out:
 	/* TODO 2: unregister block device in case of an error */
+	unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
 	return err;
 }
 
@@ -207,8 +226,10 @@ static void delete_block_device(struct my_block_dev *dev)
 static void __exit my_block_exit(void)
 {
 	/* TODO 2: cleanup block device using delete_block_device */
+	delete_block_device(&g_dev);
 
 	/* TODO 1: unregister block device */
+	unregister_blkdev(MY_BLOCK_MAJOR, MY_BLKDEV_NAME);
 }
 
 module_init(my_block_init);
